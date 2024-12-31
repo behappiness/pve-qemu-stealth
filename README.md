c4db0a12be925ae91c0cec1ccc3081dee744271f
Botond Lovász <botilovasz@gmail.com>
2024-12-31 05:26:20 +0100

    readme, install.sh

---
+++ README.md
@@ -0,0 +1,28 @@
+# PACKAGE CHANGES AND INSTALLATION
+
+This package has been modified to fake device names against anti-cheat detection. The package is built and installed on the following Proxmox VE version: [proxmox-ve_8.2-2.iso](https://enterprise.proxmox.com/iso/proxmox-ve_8.2-2.iso)
+
+To install this package, you can use the provided `install.sh` script. This script automates the installation process, but user interaction is required to apply the configuration.
+
+To run the installation script, execute the following command as root in your terminal:
+
+```bash
+wget -L https://raw.github.com/behappiness/pve-qemu-kvm-stealth/stable-8/install.sh
+bash install.sh
+```
+
+# Make your own changes
+
+You can make your own changes to the package by editing the files in the `pve-qemu-kvm-stealth` directory in the following manner:
+
+1. Clone the repository
+2. Install the dependencies (see in `install.sh`)
+3. Run `make submodule` to initialize and update submodules
+4. Use `quilt` to apply patches:
+   - Might need to `export QUILT_PATCHES=../debian/patches` and `export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"` for quilt to work properly
+   - `cd qemu`
+   - Apply existing patches: `quilt push -a`
+   - Make changes to the files as needed
+   - Refresh the patches: `quilt refresh`
+5. Run `make clean` and `make distclean` to clean the build directory
+6. Run `make` to build the package
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Define the branch name and package name as variables
+BRANCH_NAME="stable-8"  # Adjust this to the correct branch if needed
+PACKAGE_NAME="pve-qemu-kvm"
+REPO_NAME="pve-qemu-kvm-stealth"
+
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+echo "Updating package lists..."
+apt update -y
+
+echo "Installing necessary packages..."
+apt install -y git devscripts quilt meson check libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev libzstd-dev python3-sphinx-rtd-theme python3-venv quilt uuid-dev xfslibs-dev
+
+echo "Cloning or updating the repository..."
+if [ ! -d "$REPO_NAME" ]; then
+    git clone https://github.com/behappiness/$REPO_NAME.git
+fi
+cd $REPO_NAME
+git pull
+
+echo "Checking out the specific branch..."
+git checkout "$BRANCH_NAME"
+
+echo "Initializing and updating submodules..."
+make submodule
+
+echo "Creating a fresh build directory..."
+make clean
+make distclean
+make
+
+echo "Building the package..."
+make
+
+echo "Installing the built package..."
+dpkg -i *.deb
+
+echo "Fixing dependencies..."
+apt install -f -y
+
+echo "Freezing the package to prevent updates..."
+apt-mark hold "$PACKAGE_NAME"
+
+echo "Rebooting the system..."
+reboot
